afbbfdabf91dfb561d213e1a06d804a258e8f5de
Levi080 <leviribeiro0800@gmail.com>
2025-04-21 13:55:03 -0300

    feat: estrutura para criação de api usando aquitetura limpa

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Domain.Entities
+{
+    public class Customer
+    {
+        public Customer(string name, string email, string document)
+        {
+            Name = name;
+            Email = email;
+            Document = document;
+        }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Document { get; set; }
+    }
+}
---
+++ WebApi/Models/AddCustomer/AddCostumerInput.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models.AddCustomer
+{
+    public class AddCostumerInput
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Document { get; set; }
+    }
+}
